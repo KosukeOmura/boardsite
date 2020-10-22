c4ea2b1088060eb16bf5bbdef405394e521e6b15
KosukeOmura <social.666.k@gmail.com>
2020-10-22 21:35:27 +0900

    DB変更

--- admin.php
+++ admin.php
@@ -9,10 +9,6 @@ ini_set('error_reporting', E_ALL);
 define( 'PASSWORD', 'adminPassword');
 
 // データベースの接続情報
-// define( 'DB_HOST', 'us-cdbr-east-02.cleardb.com');
-// define( 'DB_USER', 'be96b3e314479f');
-// define( 'DB_PASS', '9979147b');
-// define( 'DB_NAME', 'heroku_f4a0f651ae45c36');
 define( 'DB_HOST', 'localhost');
 define( 'DB_USER', 'kosuke');
 define( 'DB_PASS', 'komazawataxidesu');
